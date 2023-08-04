5d04232684a31eebcc320a57f477141fe04d1521
BlackEddit <rasan.edd.p@gmail.com>
2023-08-03 18:06:15 -0600

    upload Solve -Palindrome_Number- -Algho- 'v1'

---
+++ Palindrome_Number.py
@@ -0,0 +1,50 @@
+# 9. Palindrome Number
+# Easy
+# 10.5K
+# 2.6K
+# Companies
+# Given an integer x, return true if x is a 
+# palindrome
+# , and false otherwise.
+ 
+
+# Example 1:
+
+# Input: x = 121
+# Output: true
+# Explanation: 121 reads as 121 from left to right and from right to left.
+# Example 2:
+
+# Input: x = -121
+# Output: false
+# Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+# Example 3:
+
+# Input: x = 10
+# Output: false
+# Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+#https://leetcode.com/problems/palindrome-number/description/
+
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        # If x is negative, it's not a palindrome.
+        # Also if the last digit of the number is 0, in order to be a palindrome,
+        # the first digit of the number also needs to be 0.
+        # Only 0 satisfy this property.
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+
+        revertedNumber = 0
+        while x > revertedNumber:
+            revertedNumber = revertedNumber * 10 + x % 10
+            x /= 10
+
+        # When the length is an odd number, we can get rid of the middle digit by revertedNumber/10
+        # For example when the input is 12321, at the end of the while loop we get x = 12, revertedNumber = 123,
+        # since the middle digit doesn't matter in palidrome (it will always equal to itself), we can simply get rid of it.
+        return x == revertedNumber or x == revertedNumber // 10
